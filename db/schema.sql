f260e45d48a89e84d0039c59b83d5dd86eb304ce
Christopher Mueller <christopher.mueller@cpxprojects.com>
2018-07-30 20:40:18 -0400

    initial load

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+CREATE TABLE `burgers` (
+  `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `burger_name` VARCHAR( 255 ) NOT NULL,
+  `devoured` BOOLEAN NOT Null,
+
+  PRIMARY KEY ( `id` ) 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+insert into burgers (burger_name,devoured)
+values ("black and blue",false),("cheese burger",false),("chicken burger",false);
+
+select * from burgers
